0129b8190fe5b8e74369a4857692725d594f9e70
priyadarshii786 <tathagatp918@gmail.com>
2023-12-26 01:18:13 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const http = require('http'); //now this is simply the way you import files in nodeJS, "require()" either takes a path to another file, or if you don't have a path to one of your files, you can also import a core module, like http
+
+/*
+function rqListener(req, res) {
+
+}
+http.createServer(rqListener);
+----- you don't have to explicitly create such a function though, you can also use so-called anonymous function...see below
+
+http.createServer(function (req, res) {
+
+});
+
+*/
+
+// or you can use an arrow function where you omit the function keyword and just have the two arguments followed by an equal sign and a greater sign....see below
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title> Enter Message </title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title> My first page </title></head>');
+    res.write('<body><h1> Hello from my NodeJS server! </h1></body>');
+    res.write('</html>');
+    res.end();
+});
+// above is our create server callback function
+
+server.listen(5000);
