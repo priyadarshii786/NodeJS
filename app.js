9b71220ce5138d45712e0a9d74843f7362335d0c
priyadarshii786 <tathagatp918@gmail.com>
2023-12-26 01:33:21 +0530

    Upto Lecture 33

--- app.js
+++ app.js
@@ -17,11 +17,18 @@ http.createServer(function (req, res) {
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
     if (url === '/') {
         res.write('<html>');
         res.write('<head><title> Enter Message </title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
 
