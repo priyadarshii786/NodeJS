9b4ed287b6625d8f244a28d0deda1740807b322e
priyadarshii786 <tathagatp918@gmail.com>
2024-01-04 00:22:04 +0530

    upto lect 59; last lecture 59 has installation process of expressJS

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const http = require('http');
 
-const routes = require('./routes');
+const express = require('express');  // ---- third party package
 
-const server = http.createServer(routes);
+const app = express();
+
+const server = http.createServer(app);
 
 // ------ Had we used Second alternative of exporting there in the routes.js file, these below lines could have been used 
 // console.log(routes.someText);
