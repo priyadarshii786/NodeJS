ab413d2495bbfe9c4518194545c5b1392df25550
priyadarshii786 <tathagatp918@gmail.com>
2024-01-04 00:25:47 +0530

    upto lect 59; last lecture 59 has installation process for expressJS

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 const server = http.createServer(app);
 
-// ------ Had we used Second alternative of exporting there in the routes.js file, these below lines could have been used 
+// ------ Had we used Second alternative of exporting there in the routes.js file, these below lines could have been used
 // console.log(routes.someText);
 // const server = http.createServer(routes.handler);
 
