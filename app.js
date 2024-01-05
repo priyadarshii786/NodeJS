2b05d7ef38acc9a759d163917750bc1a30238fa1
priyadarshii786 <tathagatp918@gmail.com>
2024-01-06 00:43:28 +0530

    upto lect 63; Handling different routes

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-// const http = require('http'); ------ since we have used the alternative of those two lines of codes there, that's why commenting out this thing here.
-
-const express = require('express');  // ---- third party package
+const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!!');
+app.use('/', (req, res, next) => {
+    console.log('This always runs!!');
     next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+    console.log('In the another middleware!!');
+    res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
     console.log('In the another middleware!!');
     res.send('<h1>Hello from Express!!')
 });
 
-// const server = http.createServer(app);
-// server.listen(5000);
-//----- here we are using the alternative of above written codes
+
 app.listen(5000);
