37ec430524949c36ad409f9a48931c1cdd38c52a
priyadarshii786 <tathagatp918@gmail.com>
2024-02-12 02:00:17 +0530

    upto lect 75

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes); // --- this is the filtering mechanism here in app.js and it allows us to put a common starting segment for our path which all routes in a given file use to outsource that into this app.js file
 app.use(shopRoutes);
