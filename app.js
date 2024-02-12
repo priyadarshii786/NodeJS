5b3d066e60d54e4cd746389bbc77ad9ce89027e8
priyadarshii786 <tathagatp918@gmail.com>
2024-02-13 03:15:37 +0530

    upto lect 79

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes); // --- this is the filtering mechanism here in app.js and it allows us to put a common starting segment for our path which all routes in a given file use to outsource that into this app.js file
+app.use('/admin', adminData.routes); // --- this is the filtering mechanism here in app.js and it allows us to put a common starting segment for our path which all routes in a given file use to outsource that into this app.js file
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
